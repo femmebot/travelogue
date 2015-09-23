f4065152925cb7637c453c8cd9cf76338392e7df
femmebot <femmebot@gmail.com>
2015-09-23 16:31:39 -0400

    base html file, css, js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 
 
-$('form').submit(function(e) {
+// $('form').submit(function(e) {
 
-  e.preventDefault();
+  // e.preventDefault();
 
   // Your Flickr ID
   var $flickrID = '35034351041';
+  var $flickrTag = 'Scandinavia';
 
   // the AJAX part
   // https://api.flickr.com/services/rest/?method=flickr.urls.lookupGallery&api_key=5a633d70f60f00ae9bf7ed5b5adbd67f&format=json
@@ -13,9 +14,9 @@ $('form').submit(function(e) {
   var $flickerAPI = 'http://api.flickr.com/services/feeds/photos_public.gne?id=' + $flickrID + '@N01&jsoncallback=?';
   // var $flickerAPI = 'https://api.flickr.com/services/rest/?method=flickr.urls.lookupGallery&api_key=5a633d70f60f00ae9bf7ed5b5adbd67f&format=json?jsoncallback=?';
 
-  var $searchQuery = $('input[type="search"]').val();
+  // var $flickrTag = $('input[type="search"]').val();
   var $flickrOptions = {
-    tags: $searchQuery,
+    tags: $flickrTag,
     format: "json"
   };
 
@@ -24,25 +25,27 @@ $('form').submit(function(e) {
 
   function displayPhotos(data) {
     // console.log(data);
-    var $photoHTML = '<ul>';
+    var $photoHTML = '';
+
     $.each(data.items,function(i,photo) {
-      $photoHTML += '<li>';
-      // $photoHTML += '<li class="grid-25 tablet-grid-50">';
-      $photoHTML += '<a href="' + photo.link + '" class="image">';
+
       var thumbnailURL = photo.media.m;
-      var imgURL = thumbnailURL.replace("_m.jpg", "_b.jpg");  // converts thumbnail to larger photo
-      var imgDate = photo.published;
+      var imgURL = thumbnailURL.replace("_m.jpg", "_c.jpg");  // converts thumbnail to larger photo
+      var imgDate = (photo.published).slice(0, 10);
       var imgTitle = photo.title;
-      console.log(imgURL);
+
+      $photoHTML += '<li><hr>';
+      $photoHTML += '<h3>' + imgTitle + '</h3>';
+      $photoHTML += '<p class="caption">' + imgDate + '</p>';
+      $photoHTML += '<a href="' + photo.link + '">';
+      // console.log(imgURL);
       $photoHTML += '<img src="' + imgURL + '"></a>';
-      $photoHTML += '<p>' + imgTitle + '</p>';
-      $photoHTML += '<p>' + imgDate.slice(0, 10) + '</p>';
       $photoHTML += '</li>';
     }); // end each
-    $photoHTML += '</ul>';
+    // $photoHTML += '</ul>';
     $('#photos').html($photoHTML);
   }
 
   $.getJSON($flickerAPI, $flickrOptions, displayPhotos);
 
-}); // end click
+// }); // end click
